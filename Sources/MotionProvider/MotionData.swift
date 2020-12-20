33b57cc1fe3b3d896625eaab69f8981df34e1ee7
Luis Riegger <lri@me.com>
2020-12-20 19:20:21 +0100

    Add public initializer for MotionData

--- Sources/MotionProvider/MotionData.swift
+++ Sources/MotionProvider/MotionData.swift
@@ -9,6 +9,16 @@ import Foundation
 
 /// Holds userAcceleration and rotationRate data from accelerometer and gyroscope
 public struct MotionData {
+    public init(timestamp: Date, acc_x: Double, acc_y: Double, acc_z: Double, rot_x: Double, rot_y: Double, rot_z: Double) {
+        self.timestamp = timestamp
+        self.acc_x = acc_x
+        self.acc_y = acc_y
+        self.acc_z = acc_z
+        self.rot_x = rot_x
+        self.rot_y = rot_y
+        self.rot_z = rot_z
+    }
+    
     public var timestamp : Date
     public var acc_x : Double // userAcceleration.x
     public var acc_y : Double // userAcceleration.y
